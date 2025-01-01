e204dc6104562bde1722f576db077c51415ce207
HuguesDelamare <bluekikiller74@gmail.com>
2025-01-01 22:07:55 +0100

    config

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 import os
 
 class Config:
-    BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(BASEDIR, 'app.db')}").replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://')
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
-    DEBUG = True 
+    DEBUG = True
