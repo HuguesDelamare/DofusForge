f091c8bce9e39cad61e57e31af60f24e6febccb7
HuguesDelamare <bluekikiller74@gmail.com>
2025-01-01 16:22:20 +0100

    database changes and debug in config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
 import os
+from flask import app
 
 class Config:
     BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True  # Active les logs SQL pour déboguer
+
+
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
