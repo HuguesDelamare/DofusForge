3a8b6a32f52d61ceb6227602d367eb59616d13b5
HuguesDelamare <bluekikiller74@gmail.com>
2025-01-01 16:26:03 +0100

    mistakes

--- app/config.py
+++ app/config.py
@@ -1,12 +1,8 @@
 import os
-from flask import app
 
 class Config:
-    BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'app.db')}"
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(BASEDIR, 'app.db')}").replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True  # Active les logs SQL pour déboguer
-
-
-app.config['DEBUG'] = True
-app.config['TESTING'] = True
+    SQLALCHEMY_ECHO = True
+    DEBUG = True  # Définir le mode debug ici
