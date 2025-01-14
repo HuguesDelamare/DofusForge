1b94d3a4197d3495da933e43378cc607a40ddcb7
HuguesDelamare <bluekikiller74@gmail.com>
2025-01-14 14:01:13 +0100

    Adding comments and renaming wanted route

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,20 @@ mail = Mail()
 
 @login_manager.user_loader
 def load_user(user_id):
+    """
+    Fonction pour charger l'utilisateur depuis la base de données.
+    """
     from app.models import User
     return User.query.get(int(user_id))
 
 def create_app():
+    """
+    Crée et configure l'application Flask.
+    """
+    # Charger les variables d'environnement
     load_dotenv()
+
+    # Créer l'application Flask
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
 
@@ -30,9 +39,15 @@ def create_app():
 
     # Enregistrer les blueprints
     from .routes import routes
-    from .auth_routes import auth, test_email
+    from .auth_routes import auth
+    from .bounties_routes import bounties
     app.register_blueprint(routes)
     app.register_blueprint(auth, url_prefix='/auth')
-    app.register_blueprint(test_email, url_prefix='/test')
+    app.register_blueprint(bounties, url_prefix='/bounties')
+
+    # Ajouter les commandes CLI
+    with app.app_context():
+        from app.commands import create_servers
+        app.cli.add_command(create_servers)
 
     return app
