3a6ce83bedd1a3c15c3bbcd8f55f8d72cb422456
HuguesDelamare <bluekikiller74@gmail.com>
2025-01-15 11:56:30 +0100

    new Heroku app name

--- app/config.py
+++ app/config.py
@@ -3,6 +3,8 @@ import os
 class Config:
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', '').replace('postgres://', 'postgresql://') or f"sqlite:///{os.path.join(BASEDIR, 'app.db')}"
+    if 'heroku' in SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('heroku', 'dofus-forge')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
